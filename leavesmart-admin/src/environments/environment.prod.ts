086b681cc3827a717beed9edb3d968b2bb698ca7
Hydar Hafiz <shogunix.chan@gmail.com>
2024-09-26 14:59:38 +0800

    set env for prod

---
+++ leavesmart-admin/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+// src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+    apiUrl: 'https://syarikathr.com/api', // Your Laravel API URL
+
+    viewStaffPath:'view-staff-profile',
+    
+    viewLeaveBalancePath:'view-leave-balances',
+    viewLeaveRequestPath:'get-leave-requests',
+    postLeaveRequestPath:'create-leave-request'
+  };
+  
---
+++ leavesmart-client/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+// src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+    apiUrl: 'https://syarikathr.com/api', // Your Laravel API URL
+    viewStaffPath:'view-staff-profile',
+    
+    viewLeaveBalancePath:'view-leave-balances',
+    viewLeaveRequestPath:'get-leave-requests',
+    postLeaveRequestPath:'create-leave-request'
+  };
+  
